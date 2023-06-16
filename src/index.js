38fb354871eb4274025bcefd3facb47c5036bd67
Khannakunal32 <khannakunal32@gmail.com>
2023-06-16 12:41:32 +0530

    appended chat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const AiChatBot = () => {
+    const [messages, setMessages] = useState([])
+
+    const gptKey = process.env.GPT_API_KEY
+
+    const handleSend = async (newMessage = []) => {
+        try {
+            // Get the user's message
+            const userMessage = newMessage[0];
+
+            // Add the user's message to the message state
+            setMessages((previousMessages) => GiftedChat.append(previousMessages, userMessage));
+            const messageText = userMessage.text.toLowerCase();
+            const keyword = ['job', 'hello', 'work', 'time', 'good employee', 'company']
+
+            if (!keywords.some(keyword => messageText.includes(keyword))){
+                // if the message does not contain any of the work-related  keyword, respond with default message
+                const gptMessage = {
+                    _id: new Date().getTime() + 1,
+                    text: "I'm your work assistant. Ask me about your work.",
+                    createdAt: new Date(),
+                    user: {
+                        id: 2,
+                        name: 'AI Chatbot'
+                    }
+                };
+                setMessages(previousMessages => GiftedChat.append(previousMessages, gptMessage));
+                return;
+            }
+
+            // if the message contains work-related keyword, fetch a response fromt he api and respond
+        }
+    }
   return (
     <View>
       <Text>AiChatBot</Text>
