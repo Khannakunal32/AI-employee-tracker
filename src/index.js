aab13bfe28aeac8880e6677e53b4489676f29ee7
Khannakunal32 <khannakunal32@gmail.com>
2023-06-16 12:17:54 +0530

    starting of proj

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AiChatBot = () => {
+  return (
+    <View>
+      <Text>AiChatBot</Text>
+    </View>
+  )
+}
+
+export default AiChatBot
