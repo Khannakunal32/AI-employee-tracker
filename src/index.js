9be51990484a34588213e01ea3456071b6b91df0
Khannakunal32 <khannakunal32@gmail.com>
2023-06-16 15:44:22 +0530

    added support for fixed responses

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { View, Text } from "react-native";
 import React, { useState } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import axios from "axios";
+import responses from "./response";
 
-GPT_API_KEY = 'YOUR_API_KEY'
+GPT_API_KEY = "YOUR_API_KEY";
 
 const AiChatBot = () => {
   const [messages, setMessages] = useState([]);
@@ -26,42 +27,40 @@ const AiChatBot = () => {
         "good employee",
         "company",
       ];
-
-      if (!keywords.some((keyword) => messageText.includes(keyword))) {
-        // if the message does not contain any of the work-related  keyword, respond with default message
-        const gptMessage = {
-          _id: new Date().getTime() + 1,
-          text: "I'm your work assistant. Ask me about your work.",
-          createdAt: new Date(),
-          user: {
-            id: 2,
-            name: "AI Chatbot",
-          },
-        };
-        setMessages((previousMessages) =>
-          GiftedChat.append(previousMessages, gptMessage)
+      var workResponse = "";
+      // check and return which keywrod is included in message text
+      const keyword = keywords.find((keyword) => messageText.includes(keyword));
+      
+      if (!keyword && !keywordFromResponse) {
+          workResponse = "I'm your work assistant. Ask me about your work.";
+        }
+        // if the message contains work-related keyword and present in responses object
+        const keywordFromResponse = Object.keys(responses).find((key) =>
+          messageText.includes(key)
         );
-        return;
+        if (keywordFromResponse) {
+        workResponse = responses[keywordFromResponse];
+      } else {
+        // if message not in responses object, use GPT-3 to generate response
+        const response = await axios.post(
+          "https://api.openai.com/v1/engines/text-davinci-003/completions",
+          {
+            prompt: `Work related ${messageText}`,
+            max_tokens: 150,
+            temperature: 0.2,
+            n: 1,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${GPT_API_KEY}`,
+            },
+          }
+        );
+        console.log(response.data);
+        workResponse = repsonse.data.choices[0].text.trim();
       }
 
-      // if the message contains work-related keyword, fetch a response fromt he api and respond
-      const response = await axios.post(
-        "https://api.openai.com/v1/engines/text-davinci-003/completions",
-        {
-          prompt: `Work related ${messageText}`,
-          max_tokens: 150,
-          temperature: 0.2,
-          n: 1,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            'Authorization': `Bearer ${GPT_API_KEY}`,
-          },
-        }
-      );
-      console.log(response.data);
-      const workResponse = repsonse.data.choices[0].text.trim();
       const gptMessage = {
         _id: new Date().getTime() + 1,
         text: workResponse,
@@ -81,13 +80,13 @@ const AiChatBot = () => {
   };
 
   return (
-    <View style={{ flex:1 }}>
+    <View style={{ flex: 1 }}>
       <View
         style={{
           backgroundColor: `#F5F5F5`,
           padding: 10,
           alignItems: "center",
-          justifyContent: 'center',
+          justifyContent: "center",
           borderBottomWidth: 1,
           marginTop: 40,
           marginBottom: 5,
@@ -102,7 +101,7 @@ const AiChatBot = () => {
           AI Chatbot
         </Text>
       </View>
-    <GiftedChat
+      <GiftedChat
         messages={messages}
         onSend={(newMessage) => handleSend(newMessage)}
         user={{ _id: 1 }}
---
+++ src/response.js
@@ -0,0 +1,39 @@
+// responses.js
+
+const responses = {
+    hello: "Hello! How can I assist you with your work today?",
+    job: "Job opportunities will be posted on our website. Keep an eye out!",
+    time: "Working hours are from 9 AM to 5 PM, Monday to Friday.",
+    "good employee": "Being a good employee involves being punctual, responsible, and dedicated.",
+    company: "Our company is focused on innovation and providing quality products/services.",
+    salary: "Salaries are determined based on various factors such as experience, qualifications, and job position.",
+    project: "Projects are assigned based on team collaboration and individual skills. We strive for successful project completion.",
+    deadline: "Deadlines are essential for project management and ensuring timely delivery of tasks.",
+    vacation: "Vacation time is an important aspect of work-life balance. We have a policy in place to support your vacation needs.",
+    training: "Training programs are provided to enhance skills and knowledge for personal and professional growth.",
+    feedback: "Feedback is crucial for continuous improvement. We encourage open communication and value your input.",
+    benefits: "We offer a comprehensive benefits package, including healthcare, retirement plans, and employee discounts.",
+    promotion: "Promotions are based on performance, achievements, and growth potential. We recognize and reward talent.",
+    meeting: "Meetings are scheduled to ensure effective communication and collaboration among team members.",
+    "work-life balance": "Maintaining a healthy work-life balance is essential for overall well-being. We support a balanced lifestyle.",
+    "remote work": "We offer remote work options for eligible positions. Remote work provides flexibility and convenience.",
+    workload: "Workload distribution is based on fairness and optimizing productivity. We promote a balanced workload.",
+    teamwork: "Teamwork is highly valued in our company. Collaboration and cooperation lead to successful outcomes.",
+    communication: "Effective communication is key to smooth workflow and understanding. We encourage open and transparent communication.",
+    skills: "Developing and improving skills is encouraged. We provide resources and opportunities for skill enhancement.",
+    challenges: "Challenges are opportunities for growth and learning. We believe in overcoming challenges together.",
+    mentoring: "Mentoring programs are available to support personal and professional development.",
+    leadership: "Leadership skills are valued and recognized. Leadership opportunities can arise based on performance and potential.",
+    "work environment": "We strive to provide a positive and inclusive work environment. Your well-being is important to us.",
+    goals: "Setting and achieving goals is encouraged. We support your aspirations and provide guidance.",
+    "workplace culture": "We foster a positive workplace culture based on respect, diversity, and collaboration.",
+    responsibilities: "Job responsibilities are outlined in your role description. We expect accountability and professionalism.",
+    "work ethics": "Maintaining strong work ethics is vital. We value honesty, integrity, and a strong work ethic.",
+    "career growth": "We provide opportunities for career growth and advancement. Your development is important to us.",
+    "workplace safety": "Workplace safety is a priority. We follow safety guidelines and promote a safe working environment.",
+    creativity: "We encourage creativity and innovation. Your ideas and contributions are valued.",
+    "work satisfaction": "Your job satisfaction is important to us. We strive to create a fulfilling work experience.",
+  };
+  
+  export default responses;
+  
